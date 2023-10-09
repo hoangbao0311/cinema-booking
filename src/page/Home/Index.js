733df41887001d7f25ec58bea278aceb3672d95a
Hoang Bao <lolcombination@gmail.com>
2023-10-09 12:53:29 +0700

    fix all

--- src/page/Home/Index.js
+++ src/page/Home/Index.js
@@ -9,13 +9,8 @@ const Index = () => {
   return (
     <div>
       <Header />
-<<<<<<< HEAD:src/components/Index.js
-      <Home />
-      <Footer /> 
-=======
       <Outlet />
       <Footer />
->>>>>>> master:src/page/Home/Index.js
     </div>
   );
 };
